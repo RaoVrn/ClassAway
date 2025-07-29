1e2bb2a3e40228a8ae5aeaee1019c6dfca3d2ffd
RaoVrn <prakash.varun.0305@gmail.com>
2025-07-29 16:31:11 +0530

    code refactor

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  envPrefix: 'VITE_', // Ensures only variables starting with VITE_ are injected
 });
