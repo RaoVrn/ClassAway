52c28bfdeaea316b63e32e9aea7d99a1d3ae06f1
RaoVrn <prakash.varun.0305@gmail.com>
2025-07-29 14:52:27 +0530

    minor fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __DEFINES__: '{}',
-    __HMR_CONFIG_NAME__: '""',
-    __SERVER_HOST__: '""',
-    __BASE__: '""',
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-  },
-  envPrefix: 'VITE_',
+  envPrefix: 'VITE_', // Ensures only variables starting with VITE_ are injected
 });
